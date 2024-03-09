90c5136f3538988d1625ca307bc28419adedf809
mykola.diahovets <mykola.diahovets@digicert.com>
2024-03-09 21:11:01 +0200

    add swift-format

--- Package.swift
+++ Package.swift
@@ -4,33 +4,36 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftUI",
-    products: [
-        .executable(name: "SwiftUI", targets: ["SwiftUI"])
-    ],
-    dependencies: [
-      .package(url: "https://github.com/IIMrFreemanII/CVulkan", branch: "main"),
-      .package(url: "https://github.com/IIMrFreemanII/CGLFW3", branch: "main"),
-      .package(url: "https://github.com/IIMrFreemanII/SwiftGLM", branch: "main"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "SwiftUI",
-            dependencies: [
-                "CGLFW3", 
-                "SwiftGLM",
-                "CVulkan"
-            ],
-            cSettings: [
-                .unsafeFlags(["-I", "C:\\VulkanSDK\\1.3.275.0\\Include"], .when(platforms: [.windows])),
-                .define("GLFW_INCLUDE_VULKAN")
-            ],
-            linkerSettings: [
-                .unsafeFlags(["-Xlinker", "-rpath", "-Xlinker", "/usr/local/lib", "-lvulkan"], .when(platforms: [.macOS])),
-                .unsafeFlags(["-L", "C:\\VulkanSDK\\1.3.275.0\\Lib", "-lvulkan-1"], .when(platforms: [.windows]))
-            ] 
-        ),
-    ]
+  name: "SwiftUI",
+  products: [
+    .executable(name: "SwiftUI", targets: ["SwiftUI"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/IIMrFreemanII/CVulkan", branch: "main"),
+    .package(url: "https://github.com/IIMrFreemanII/CGLFW3", branch: "main"),
+    .package(url: "https://github.com/IIMrFreemanII/SwiftGLM", branch: "main"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "SwiftUI",
+      dependencies: [
+        "CGLFW3",
+        "SwiftGLM",
+        "CVulkan",
+      ],
+      cSettings: [
+        .unsafeFlags(["-I", "C:\\VulkanSDK\\1.3.275.0\\Include"], .when(platforms: [.windows])),
+        .define("GLFW_INCLUDE_VULKAN"),
+      ],
+      linkerSettings: [
+        .unsafeFlags(
+          ["-Xlinker", "-rpath", "-Xlinker", "/usr/local/lib", "-lvulkan"],
+          .when(platforms: [.macOS])),
+        .unsafeFlags(
+          ["-L", "C:\\VulkanSDK\\1.3.275.0\\Lib", "-lvulkan-1"], .when(platforms: [.windows])),
+      ]
+    )
+  ]
 )
--- Sources/SwiftUI/Application.swift
+++ Sources/SwiftUI/Application.swift
@@ -5,19 +5,63 @@ class Application {
   let WIDTH: Int32 = 800
   let HEIGHT: Int32 = 600
   var window: OpaquePointer?
+  var instance: VkInstance?
+  var validationLayers = [
+    "VK_LAYER_KHRONOS_validation".cString
+  ]
+
+  #if DEBUG
+    var enableValidationLayers = true
+  #else
+    var enableValidationLayers = false
+  #endif
 
   func run() {
-    initWindow();
-    initVulkan();
-    mainLoop();
-    cleanup();
+    initWindow()
+    initVulkan()
+    mainLoop()
+    cleanup()
   }
 
   func initVulkan() {
     createInstance()
   }
 
+  func checkValidationLayerSupport() -> Bool {
+    var layerCount: UInt32 = 0
+    vkEnumerateInstanceLayerProperties(&layerCount, nil)
+    var availableLayers = Array(repeating: VkLayerProperties(), count: Int(layerCount))
+    vkEnumerateInstanceLayerProperties(&layerCount, &availableLayers)
+
+    for layerName in self.validationLayers {
+      var layerFound = false
+
+      for var layerProperties in availableLayers {
+        let item = withUnsafePointer(to: &layerProperties.layerName) {
+          $0.withMemoryRebound(to: CChar.self, capacity: Int(VK_MAX_EXTENSION_NAME_SIZE)) {
+            String(cString: $0)
+          }
+        }
+
+        if item == layerName.string {
+          layerFound = true
+          break
+        }
+      }
+
+      if !layerFound {
+        return false
+      }
+    }
+
+    return true
+  }
+
   func createInstance() {
+    if self.enableValidationLayers && !checkValidationLayerSupport() {
+      fatalError("Validation layers requested, but not available!")
+    }
+
     var appInfo = VkApplicationInfo()
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO
     appInfo.pApplicationName = "Hello Vulkan".cString
@@ -54,14 +98,13 @@ class Application {
     createInfo.pApplicationInfo = withUnsafePointer(to: &appInfo) { $0 }
     createInfo.enabledExtensionCount = UInt32(requiredExtensions.count)
     createInfo.enabledLayerCount = 0
-    createInfo.ppEnabledExtensionNames = requiredExtensions.withUnsafeBufferPointer { $0 }.baseAddress
-    createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR.rawValue  
+    createInfo.ppEnabledExtensionNames =
+      requiredExtensions.withUnsafeBufferPointer { $0 }.baseAddress
+    createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR.rawValue
 
-    var instance: VkInstance?
-
-    let result = vkCreateInstance(&createInfo, nil, &instance)
+    let result = vkCreateInstance(&createInfo, nil, &self.instance)
     if result != VK_SUCCESS {
-        fatalError("Failed to create Vulkan instance: \(result)")
+      fatalError("Failed to create Vulkan instance: \(result)")
     }
 
     // Query the number of available instance extensions
@@ -75,33 +118,34 @@ class Application {
 
     // Print the available extensions
     for i in 0..<Int(extensionCount) {
-        let extensionName = withUnsafePointer(to: &extensions[i].extensionName) {
-            $0.withMemoryRebound(to: CChar.self, capacity: Int(VK_MAX_EXTENSION_NAME_SIZE)) {
-                String(cString: $0)
-            }
+      let extensionName = withUnsafePointer(to: &extensions[i].extensionName) {
+        $0.withMemoryRebound(to: CChar.self, capacity: Int(VK_MAX_EXTENSION_NAME_SIZE)) {
+          String(cString: $0)
         }
-        print("  \(i + 1): \(extensionName)")
+      }
+      print("  \(i + 1): \(extensionName)")
     }
   }
 
   func initWindow() {
-    glfwInit();
+    glfwInit()
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API)
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE)
 
-    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nil, nil);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nil, nil)
   }
 
   func mainLoop() {
-    while (glfwWindowShouldClose(window) == GLFW_FALSE) {
-        glfwPollEvents();
+    while glfwWindowShouldClose(window) == GLFW_FALSE {
+      glfwPollEvents()
     }
   }
 
   func cleanup() {
-    glfwDestroyWindow(window);
+    vkDestroyInstance(self.instance, nil)
+    glfwDestroyWindow(self.window)
 
-    glfwTerminate();
+    glfwTerminate()
   }
 }
